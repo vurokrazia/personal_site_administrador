0df6d74f3959ce43e4172d578d07b977a3187b13
jesus <jesus.alberto.vk@gmail.com>
2020-07-09 19:10:25 -0500

    deploy

---
+++ dist/precache-manifest.14a2d63113c59d0a835e33b5e19250b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a8b69d7473e6337b48",
+    "url": "/css/app.c42cc161.css"
+  },
+  {
+    "revision": "0113f4780f28ddf1aab1",
+    "url": "/css/chunk-vendors.c1f129b8.css"
+  },
+  {
+    "revision": "2659418c7522d677c721e70acb795c5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50a8b69d7473e6337b48",
+    "url": "/js/app.886f2068.js"
+  },
+  {
+    "revision": "0113f4780f28ddf1aab1",
+    "url": "/js/chunk-vendors.1dee6db7.js"
+  },
+  {
+    "revision": "2fbd281480b3caa4e2928c36dea3623f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
